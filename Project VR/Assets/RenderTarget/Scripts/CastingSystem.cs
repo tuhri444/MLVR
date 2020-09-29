ce408aad15c822df54c70b8227aa6aad50455280
Arjen de Aldrey <adealdrey3@gmail.com>
2020-09-29 16:43:41 +0200

    [ADD] started working on delegate system

---
+++ Project VR/Assets/RenderTarget/Scripts/CastingSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CastingSystem : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The XR controllers")]
+    List<XRController> controllers;
+    /// <summary>
+    /// The XR controllers.
+    /// </summary>
+    public List<XRController> Controllers { get { return controllers; } set { controllers = value; } }
+
+    private bool drawingMode;
+
+    public delegate void OnDrawingMode();
+    public delegate void OnDrawingStart();
+    public delegate void OnDrawingStop();
+
+    private void CheckDrawingMode()
+    {
+        foreach(XRController controller in controllers)
+        {
+
+        }
+    }
+
+}
